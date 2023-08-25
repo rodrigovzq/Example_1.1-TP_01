a432180e25eb07336aa6a7d7b565ab0985d5f83b
Rodrigo Vazquez <rvazquez@fi.uba.ar>
2023-08-25 00:22:30 +0000

    agrego comentarios doxygen

--- main.cpp
+++ main.cpp
@@ -3,22 +3,22 @@
 
 int main()
 {
-    DigitalIn gasDetector(D2); //!
+    DigitalIn gasDetector(D2); //! Creates a gasDetector digital input object in the D2 pin
 
-    DigitalOut alarmLed(LED1);
+    DigitalOut alarmLed(LED1); //! Creates an alarmLed digital output object in LED1 
 
-    gasDetector.mode(PullDown);
+    gasDetector.mode(PullDown); //! Assigns a Pulldown internal resistor to the gasDetector object
 
-    alarmLed = OFF;
+    alarmLed = OFF; //! Assigns an OFF state (from the arm_book_lib library) to the alarmLed object
     
     while (true) {
-        printf("%s\n","Hello World");
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
-        }
+        printf("%s\n","Hello World"); //! prints a simple message to test the console output
+        if ( gasDetector == ON ) {    //! Condition to detect the digitalinput state
+            alarmLed = ON;             //! The LED turns on if the digitalinput has an ON state
+        }       
         
         if ( gasDetector == OFF ) {
-            alarmLed = OFF;
+            alarmLed = OFF;         //! The LED turns OFF if the digitalinput has an OFF state
         }
     }
 }
